29c054fb634ab92070bbb24b026eef7e651a7837
Peter Luladjiev <luladjiev@gmail.com>
2022-04-29 22:04:06 +0300

    Update lgit to use correct default branch

--- lib/lgit.rb
+++ lib/lgit.rb
@@ -4,23 +4,33 @@ module Lgit
   ###
   # Git related class
   class Git
-    def refresh_base(base = 'master')
+    def refresh_base(base)
+      base ||= get_default_branch
+
       `git checkout #{base}`
       `git pull`
     end
 
     def get_branch
+      `git branch --show-current`.strip
+    end
+
+    def get_default_branch
       `git name-rev --name-only HEAD`.strip
     end
 
-    def create_branch(name, base = 'master')
+    def create_branch(name, base)
       return unless name
+
+      base ||= get_default_branch
 
       refresh_base base
       `git checkout -b #{name}`
     end
 
-    def rebase(base = 'master')
+    def rebase(base)
+      base ||= get_default_branch
+
       refresh_base base
       `git checkout - `
       `git rebase #{base}`
